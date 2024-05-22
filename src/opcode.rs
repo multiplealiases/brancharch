a33c06de499505622689855127bd52439880889e
multiplealiases <conflictvegetable@protonmail.com>
2024-05-23 02:01:44 +0800

    use num_enum to generate opcode boilerplate for me

--- src/opcode.rs
+++ src/opcode.rs
@@ -1,78 +1,8 @@
-impl From<u8> for OpCode {
-    fn from(byte: u8) -> OpCode {
-        match byte {
-            0  => OpCode::Nop,
-
-            1  => OpCode::LoadA,
-            2  => OpCode::LoadB,
-            3  => OpCode::LoadC,
-            4  => OpCode::LoadD,
-
-            5  => OpCode::StoreA,
-            6  => OpCode::StoreB,
-            7  => OpCode::StoreC,
-            8  => OpCode::StoreD,
-
-            9  => OpCode::SetFlag,
-            10 => OpCode::ClearFlag,
-
-            11 => OpCode::AddAA,
-            12 => OpCode::AddBA,
-            13 => OpCode::AddCA,
-            14 => OpCode::AddDA,
-            15 => OpCode::AddBB,
-            16 => OpCode::AddBC,
-            17 => OpCode::AddBD,
-            18 => OpCode::AddCB,
-            19 => OpCode::AddCC,
-            20 => OpCode::AddCD,
-            21 => OpCode::AddDB,
-            22 => OpCode::AddDC,
-            23 => OpCode::AddDD,
-
-            24 => OpCode::SubAA,
-            25 => OpCode::SubBA,
-            26 => OpCode::SubCA,
-            27 => OpCode::SubDA,
-            28 => OpCode::SubBB,
-            29 => OpCode::SubBC,
-            30 => OpCode::SubBD,
-            31 => OpCode::SubCB,
-            32 => OpCode::SubCC,
-            33 => OpCode::SubCD,
-            34 => OpCode::SubDB,
-            35 => OpCode::SubDC,
-            36 => OpCode::SubDD,
-
-            37 => OpCode::Jmp,
-            38 => OpCode::JmpRel,
-            39 => OpCode::WriteIp,
-
-            40 => OpCode::Branch,
-
-            41 => OpCode::LessThanA,
-            42 => OpCode::GreaterThanA, 
-            43 => OpCode::EqualToA,
-            44 => OpCode::LessThanB,
-            45 => OpCode::GreaterThanB,
-            46 => OpCode::EqualToB,
-            47 => OpCode::LessThanC,
-            48 => OpCode::GreaterThanC,
-            49 => OpCode::EqualToC,
-            50 => OpCode::LessThanD,
-            51 => OpCode::GreaterThanD,
-            52 => OpCode::EqualToD,            
-
-            53 => OpCode::Print,
-            0xFF => OpCode::AttemptHalt,
-            _ => OpCode::Nop
-        }
-    }
-}
-
+use num_enum::FromPrimitive;
 #[repr(u8)]
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, FromPrimitive)]
 pub enum OpCode {
+    #[num_enum(default)]
     Nop,
 
     LoadA, LoadB, LoadC, LoadD,
